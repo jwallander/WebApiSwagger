2c1156f985df7bacd05ebac36005d28b9dac1f04
Jonas Wallander <jwallander@gmail.com>
2016-10-30 18:22:09 +0100

    NSwag generated client

---
+++ ConsoleClientNSwag/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WebApiSwaggerClient;
+
+namespace ConsoleClientNSwag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new StudentsClient();
+
+            var observableStudents = client.GetAllAsync().Result;
+            
+            foreach (var student in observableStudents)
+            {
+                Console.WriteLine(student.ToJson());
+            }
+        }
+    }
+}
